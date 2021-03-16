e3c9af162b820091da204b5cb59a5d3aceb378be
sabitalievanuskaiym <77196324+Sabitalieva@users.noreply.github.com>
2021-03-16 15:29:03 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+        public class Main {
+
+            public static void main(String[] args)  {
+                BankAccount bankAccount = new BankAccount();
+                bankAccount.deposit(20000);
+                System.out.println("На вашем счете: " + bankAccount.getAmount() + " coм.");
+                System.out.println("Сколько денег хотите снять ?");
+                Scanner scanner = new Scanner(System.in);
+                int m = scanner.nextInt();
+
+
+                while (true) {
+                    try {
+                        System.out.println(bankAccount.getAmount());
+                        bankAccount.withdraw(m);
+                    } catch (LimitException e) {
+                        System.out.println(e.getMessage());
+                        try {
+                            bankAccount.withdraw((int) bankAccount.getAmount());
+                        } catch (LimitException limitException) {
+                            limitException.printStackTrace();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
